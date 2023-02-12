50c41040c87783a95399adba0aa176ba7476db08
gunnvrs <gunn2545@gmail.com>
2023-02-12 22:47:34 +0700

    move from html to .js and add Navbar

--- src/Appauth.js
+++ src/Appauth.js
@@ -10,6 +10,7 @@ function refreshPage() {
 function Appauth() {
   return (
     <div className="Appauth">
+      
       <button class="login-with-google-btn" onClick={signInWithGoogle}>
         Sign in with Google
       </button>
@@ -19,7 +20,15 @@ function Appauth() {
           refreshPage();
         }}>refresh
       </button> */}
+      <line1></line1>
+    <line2></line2>
+    <email>email</email>
+    <password>name</password>
+    <h2>ItzMine</h2>
 
+    <googleemailcover></googleemailcover>
+    <googlenamecover></googlenamecover>
+    <a href="main.html" class="button">login</a>
       <googleinname2>{localStorage.getItem("name")}</googleinname2>
       <googleinemail2>{localStorage.getItem("email")}</googleinemail2>
       <img src={localStorage.getItem("profilePic")} />
