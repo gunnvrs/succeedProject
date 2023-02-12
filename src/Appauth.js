54b30df0f0d1f31549d94cef050279969cbfdc41
gunnvrs <gunn2545@gmail.com>
2023-02-12 22:14:43 +0700

    another test commit

--- src/Appauth.js
+++ src/Appauth.js
@@ -1,15 +1,30 @@
 import "./App.css";
 import { signInWithGoogle } from "./Firebase";
 
+function refreshPage() {
+  if (signInWithGoogle)  {
+    window.location.reload();
+  }
+}
+
 function Appauth() {
   return (
     <div className="Appauth">
       <button class="login-with-google-btn" onClick={signInWithGoogle}>
         Sign in with Google
       </button>
+
+      {/* <button class="login-with-google-btn" onClick={() => {
+          signInWithGoogle();
+          refreshPage();
+        }}>refresh
+      </button> */}
+
       <googleinname2>{localStorage.getItem("name")}</googleinname2>
       <googleinemail2>{localStorage.getItem("email")}</googleinemail2>
       <img src={localStorage.getItem("profilePic")} />
+      <coverlogin></coverlogin>
+
     </div>
   );
 }
